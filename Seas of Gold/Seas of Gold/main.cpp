dd6a4a567147d3796dfc1537126dc8d03ba9b261
niceguy4704 <niceguy4704@yahoo.com>
2016-04-03 22:04:51 -0400

    texture and models
    gfcg

--- Seas of Gold/Seas of Gold/main.cpp
+++ Seas of Gold/Seas of Gold/main.cpp
@@ -41,7 +41,8 @@ int main()
 	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
 		rect<s32>(10, 10, 260, 22), true);
 
-	IAnimatedMesh* mesh = smgr->getMesh("sydney.md2");
+	//IAnimatedMesh* mesh = smgr->getMesh("sydney.md2");
+	IAnimatedMesh* mesh = smgr->getMesh("textures/map.3ds");
 	if (!mesh)
 	{
 		device->drop();
@@ -53,7 +54,8 @@ int main()
 	{
 		node->setMaterialFlag(EMF_LIGHTING, false);
 		node->setMD2Animation(scene::EMAT_STAND);
-		node->setMaterialTexture(0, driver->getTexture("sydney.bmp"));
+		//node->setMaterialTexture(0, driver->getTexture("textures/sydney.bmp"));
+		//node->setMaterialTexture(0, driver->getTexture("textures/map.png"));
 	}
 	smgr->addCameraSceneNode(0, vector3df(0, 30, -40), vector3df(0, 5, 0));
 
