8fc66d53f14d7a0629710ed97d80520ed01a574f
niceguy4704 <niceguy4704@yahoo.com>
2016-06-10 15:41:57 -0400

    Medium Update
    - rebuilt england
    - counters are now separate objects
    - bugs squashed in map loader
    - linker bug fixed
    -palm trees scale fixed
    - added support for Africa

--- Seas of Gold/Seas of Gold/LoadMap.h
+++ Seas of Gold/Seas of Gold/LoadMap.h
@@ -3,22 +3,37 @@
 enum MapID
 {
 	Map_England = 0,
-	Map_India = 1
+	Map_India = 1,
+	Map_Africa = 2
 };
-const int MAP_COUNT = 2;
+const int MAP_COUNT = 3;
 struct MapData
 {
 	irrstring* MapFiles = new irrstring[MAP_COUNT];
 	irrstring* MapAssets = new irrstring[MAP_COUNT];
 	irrstring* MapMerchants = new irrstring[MAP_COUNT];
-	void Initialize(){
-		MapFiles[Map_England] = "Assets/map.3ds";
+	irrstring* MapVendorTable = new irrstring[MAP_COUNT];
+	irrstring* MapCraftingTable = new irrstring[MAP_COUNT];
+	
+	void Initialize()
+	{
+		MapFiles[Map_England] = "Assets/enmap.3ds";
 		MapAssets[Map_England] = "Assets/trees_en.3ds";
 		MapMerchants[Map_England] = "Assets/enMerch.x";
+		MapVendorTable[Map_England] = "Assets/vndrTble.3ds";
+		MapCraftingTable[Map_England] = "Assets/crftTble.3ds";
 
 		MapFiles[Map_India] = "Assets/indMap.3ds";
+		MapAssets[Map_India] = "Assets/trees_in.3ds";
 		MapMerchants[Map_India] = "Assets/inMerch.x";
-		MapAssets[Map_India] = "Assets/trees_en.3ds";
+		MapVendorTable[Map_India] = "Assets/vndrTble.3ds";
+		MapCraftingTable[Map_India] = "Assets/crftTble.3ds";
+
+		MapFiles[Map_Africa] = "Assets/afMap.3ds";
+		MapAssets[Map_Africa] = "Assets/trees_in.3ds";
+		MapMerchants[Map_Africa] = "Assets/inMerch.x";
+		MapVendorTable[Map_Africa] = "Assets/vndrTble.3ds";
+		MapCraftingTable[Map_Africa] = "Assets/crftTble.3ds";
 	}
 };
 
@@ -27,15 +42,11 @@ class LoadMap
 	MapData mapData;
 public:
 	IMeshSceneNode* seasNode;
-<<<<<<< HEAD
-	void england(ISceneManager* smgr, IrrlichtDevice *device);
-	void india(ISceneManager* smgr, IrrlichtDevice *device);
-	void africa(ISceneManager* smgr, IrrlichtDevice *device);
-=======
 	void Load(ISceneManager* smgr, IrrlichtDevice *device, int map);
 	LoadMap()
 	{
 		mapData.Initialize();
 	}
->>>>>>> refs/remotes/OmniPlatGames/master
+
+
 };
